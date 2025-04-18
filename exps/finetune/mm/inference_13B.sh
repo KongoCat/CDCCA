39b0a53f77851611a98b5bf712234678c7846ab6
Guanqun Wang <506045677@qq.com>
2025-04-18 13:58:21 +0800

    Update inference_13B.sh
    Extended wait time in the cloud

--- exps/finetune/mm/inference_13B.sh
+++ exps/finetune/mm/inference_13B.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+export NCCL_BLOCKING_WAIT=1
+export NCCL_ASYNC_ERROR_HANDLING=1
+export NCCL_TIMEOUT=1800
+export NCCL_IB_DISABLE=1
+
 PRETRAINED=/home/cx/llama2_accessory/LLaMA2-Accessory-main/output/common/llama2_qformer_13B_aokvqa/epoch2
 PRETRAINED_BASE=/home/cx/ckpts/llama2_acc/alpacaLlava_llamaQformerv2Peft_13b
 LLAMA_CONFIG="/home/cx/llama2_accessory/LLaMA2-Accessory-main/accessory/configs/13B_params.json"
