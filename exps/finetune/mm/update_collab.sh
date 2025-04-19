bd4414f7863ae34b3414b909325826cb4b58ee07
Guanqun Wang <506045677@qq.com>
2025-04-19 14:16:30 +0800

    Update update_collab.sh

--- exps/finetune/mm/update_collab.sh
+++ exps/finetune/mm/update_collab.sh
@@ -11,7 +11,7 @@ model_parallel=1
 
 exp_name=cdccav2/debug
 echo "exp name: $exp_name"
-mkdir -p /home/cx/ckpts/TPAMI/"$exp_name"
+mkdir -p /path_to_save/"$exp_name"
 
 CUDA_VISIBLE_DEVICES=6 torchrun --master_port=1112 --nproc_per_node=1 /home/cx/llama2_accessory/LLaMA2-Accessory-main/accessory/main_finetune_CDCCA.py \
 --output_dir /home/cx/ckpts/TPAMI/"$exp_name" --epochs 3 --warmup_epochs 0.2 \
@@ -20,9 +20,9 @@ CUDA_VISIBLE_DEVICES=6 torchrun --master_port=1112 --nproc_per_node=1 /home/cx/l
 --lr 0.00003 --min_lr 0.000005 --clip_grad 2 --weight_decay 0.02 \
 --data_parallel "$data_parallel" --model_parallel_size "$model_parallel"  --checkpointing \
 --llama_type llama_qformerv2_cloud --llama_config "/home/cx/llama2_accessory/LLaMA2-Accessory-main/accessory/configs/7B_params.json" --tokenizer_path "/home/cx/llama2_accessory/LLaMA2-Accessory-main/accessory/configs/tokenizer.model" \
---pretrained_path "/home/cx/ckpts/llama2_acc/alpacaLlava_llamaQformerv2" --pretrained_type="$pretrained_type" \
+--pretrained_base "/path_to_pretrained_base/alpacaLlava_llamaQformerv2" --pretrained_path "/path_to_pretrained_lora" --pretrained_type="$pretrained_type" \
 --data_config $data_config \
 --only_save_trainable \
-2>&1 | tee -a /home/cx/ckpts/TPAMI/"$exp_name"/output.log
+2>&1 | tee -a /path_to_save/"$exp_name"/output.log
 
 echo "exp name: $exp_name"
