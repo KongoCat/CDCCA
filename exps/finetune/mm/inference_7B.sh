dbcf82637e4b92b17e13f37365741f7937cbedb5
Guanqun Wang <506045677@qq.com>
2025-04-20 07:21:39 +0800

    Update inference_7B.sh

--- exps/finetune/mm/inference_7B.sh
+++ exps/finetune/mm/inference_7B.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 PRETRAINED=/path_to_save_lora/epoch0
 PRETRAINED_BASE=/path_to_save/alpacaLlava_llamaQformerv2
-LLAMA_CONFIG="/home/cx/llama2_accessory/LLaMA2-Accessory-main/accessory/configs/7B_params.json"
-TOKENIZER=/home/cx/llama2_accessory/LLaMA2-Accessory-main/accessory/configs/tokenizer.model
+LLAMA_CONFIG="/path_to_your_accessory/configs/7B_params.json"
+TOKENIZER=/path_to_your_accessory/LLaMA2-Accessory-main/accessory/configs/tokenizer.model
 
 
 data_parallel=fsdp
 model_parallel=1
 
-CUDA_VISIBLE_DEVICES=4 torchrun --nproc-per-node=1 --master-port=11112 /home/cx/llama2_accessory/LLaMA2-Accessory-main/accessory/demos/single_turn_mm_edge.py \
+CUDA_VISIBLE_DEVICES=4 torchrun --nproc-per-node=1 --master-port=11112 /path_to_your_accessory/demos/single_turn_mm_edge.py \
 --pretrained_path $PRETRAINED --pretrained_path_base $PRETRAINED_BASE --llama_type llama_qformerv2_edge --llama_config $LLAMA_CONFIG --tokenizer_path $TOKENIZER
